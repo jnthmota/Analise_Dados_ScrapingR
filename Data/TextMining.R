226b2a4ec26ade62fea381501e0f84ceca9182dd
jnthmota <jonathan.mota@outlook.com>
2021-06-18 19:51:02 -0300

    preparação

--- Data/TextMining.R
+++ Data/TextMining.R
@@ -30,6 +30,31 @@ freq(dat1)   # gerando gráficos para variáveis categóricas, pacote funModeling
 plot_num(dat1) # gerando gráficos para variáveis numéricas, pacote funModeling
 
 
-#Analisando dados Ausentes
+#Imputando dados ausentes
+
+#Imputação simples
+dados_imputados = dat1 #DF temporário
+colSums(is.na(dados_imputados))
+which(is.na(dat1[,1]))
+dat1[which(is.na(dat1[,1])),]
+mean(dados_imputados$V1,  na.rm = TRUE)
+dados_imputados$V1 = impute(dados_imputados$V1, mean) #Imputação pacote hmisc
+mean(dados_imputados$V1)
+colSums(is.na(dados_imputados))
+
+#imputação múltipla
+dados_imputados = dat1 #DF temporário
+impute <- mice(dados_imputados, m=3, seed = 123) #Imputação pacote mice
+print(impute)
+impute$imp$V4
+dados_imputados <- complete(impute, 1)
+colSums(is.na(dados_imputados))
+
+#Descartando dados ausentes
+dados_completos = dat1[complete.cases(dat1), ]
+colSums(is.na(dados_completos))
+dim(dat1)
+dim(dados_completos)
+
 
 
